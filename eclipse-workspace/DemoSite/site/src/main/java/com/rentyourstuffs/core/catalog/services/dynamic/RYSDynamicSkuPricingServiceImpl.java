5fab1078a8b4de58cec16ca4d2b8b2aab7a4405f
vishal <vishal.durgad@ril.com>
2016-04-10 20:53:31 +0530

    cart Update rental term fixed | style - menu and others

--- eclipse-workspace/DemoSite/site/src/main/java/com/rentyourstuffs/core/catalog/services/dynamic/RYSDynamicSkuPricingServiceImpl.java
+++ eclipse-workspace/DemoSite/site/src/main/java/com/rentyourstuffs/core/catalog/services/dynamic/RYSDynamicSkuPricingServiceImpl.java
@@ -18,17 +18,17 @@ import org.broadleafcommerce.core.order.domain.OrderItem;
 import org.springframework.stereotype.Service;
 import org.broadleafcommerce.core.web.order.CartState;
 
+import com.rentyourstuffs.core.order.domain.RYSOrderItem;
 import com.rentyourstuffs.customService.product.RYSRentalTermService;
 import com.rentyourstuffs.customentities.RYSRentalTerm;
-
 
 @Service("rysDynamicSkuPricingService")
 public class RYSDynamicSkuPricingServiceImpl implements
 		DynamicSkuPricingService {
 
 	@Resource(name = "rysRentalService")
-	RYSRentalTermService  rentalTermService;
-	
+	RYSRentalTermService rentalTermService;
+
 	@Override
 	public DynamicSkuPrices getPriceAdjustment(ProductOptionValueImpl arg0,
 			Money arg1, HashMap arg2) {
@@ -43,53 +43,78 @@ public class RYSDynamicSkuPricingServiceImpl implements
 		return null;
 	}
 
-	/* (non-Javadoc)
-	 * @see org.broadleafcommerce.core.catalog.service.dynamic.DynamicSkuPricingService#getSkuPrices(org.broadleafcommerce.core.catalog.domain.Sku, java.util.HashMap)
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * org.broadleafcommerce.core.catalog.service.dynamic.DynamicSkuPricingService
+	 * #getSkuPrices(org.broadleafcommerce.core.catalog.domain.Sku,
+	 * java.util.HashMap)
 	 */
 	@Override
-	public DynamicSkuPrices getSkuPrices(Sku sku, @SuppressWarnings("rawtypes") HashMap skuPricingConsiderations) {
-		
+	public DynamicSkuPrices getSkuPrices(Sku sku,
+			@SuppressWarnings("rawtypes") HashMap skuPricingConsiderations) {
+
 		Money retailPrice = null;
-        Money salePrice = null;
-        DynamicSkuPrices prices = new DynamicSkuPrices();
-        if(skuPricingConsiderations != null && skuPricingConsiderations.get(sku.getId().toString()) != null)
-        {
-        	String rentTerm = (String)skuPricingConsiderations.get(sku.getId().toString());
-        	if(rentTerm != null)
-        	{
-        		List<RYSRentalTerm> termItemByduration = rentalTermService.getTermItemByduration(rentTerm);
-        		if(termItemByduration != null && !termItemByduration.isEmpty())
-        		{
-        			RYSRentalTerm rysRentalTerm = termItemByduration.get(0);
-        			int termPercentage = rysRentalTerm.getTermPercentage();
-        			Money actualPrice = (Money)skuPricingConsiderations.get("defaultPrice");
-        			BigDecimal termPrice = new BigDecimal((actualPrice.getAmount().doubleValue() * termPercentage) / 100);
-        			salePrice = new Money(termPrice);
-        			retailPrice = new Money(termPrice);
-        			prices.setSalePrice(salePrice);
-        			prices.setRetailPrice(retailPrice);
-        			return prices;
-        		}
-        	}
-        }
-        //if it is new item
-        
-        Order cart = CartState.getCart();
-        List<OrderItem> orderItems = cart.getOrderItems();
-        for(OrderItem orderItem : orderItems)
-        {
-        	if(orderItem instanceof DiscreteOrderItem)
-        	{
-        		DiscreteOrderItem disItem = (DiscreteOrderItem)orderItem;
-        		if(disItem.getSku().getId().equals(sku.getId()))
-        		{
-        			prices.setRetailPrice(disItem.getRetailPrice());
-        	        prices.setSalePrice(disItem.getSalePrice());
-        		}
-        	}
-        }
-        
-        return prices;
+		Money salePrice = null;
+		DynamicSkuPrices prices = new DynamicSkuPrices();
+		if (skuPricingConsiderations != null
+				&& skuPricingConsiderations.get(sku.getId().toString()) != null) {
+			String rentTerm = (String) skuPricingConsiderations.get(sku.getId()
+					.toString());
+			if (rentTerm != null) {
+				List<RYSRentalTerm> termItemByduration = rentalTermService
+						.getTermItemByduration(rentTerm);
+				if (termItemByduration != null && !termItemByduration.isEmpty()) {
+					RYSRentalTerm rysRentalTerm = termItemByduration.get(0);
+					int termPercentage = rysRentalTerm.getTermPercentage();
+					Money actualPrice = (Money) skuPricingConsiderations
+							.get("defaultPrice");
+					BigDecimal termPrice = new BigDecimal((actualPrice
+							.getAmount().doubleValue() * termPercentage) / 100);
+					salePrice = new Money(termPrice);
+					retailPrice = new Money(termPrice);
+					prices.setSalePrice(salePrice);
+					prices.setRetailPrice(retailPrice);
+					setUpdatedRentalTermAtCart(rentTerm, sku.getId());
+					return prices;
+				}
+			}
+		}
+		// if it is new item
+
+		Order cart = CartState.getCart();
+		List<OrderItem> orderItems = cart.getOrderItems();
+		for (OrderItem orderItem : orderItems) {
+			if (orderItem instanceof DiscreteOrderItem) {
+				DiscreteOrderItem disItem = (DiscreteOrderItem) orderItem;
+				if (disItem.getSku().getId().equals(sku.getId())) {
+					prices.setRetailPrice(disItem.getRetailPrice());
+					prices.setSalePrice(disItem.getSalePrice());
+				}
+			}
+		}
+
+		return prices;
+	}
+
+	/**
+	 * @param rentTerm
+	 *            Setting the updated rental terms in the cart page
+	 * @param skuId
+	 */
+	protected void setUpdatedRentalTermAtCart(String rentTerm, Long skuId) {
+		Order cart = CartState.getCart();
+		List<OrderItem> orderItems = cart.getOrderItems();
+		for (OrderItem orderItem : orderItems) {
+			if (orderItem instanceof DiscreteOrderItem) {
+				DiscreteOrderItem disItem = (DiscreteOrderItem) orderItem;
+				RYSOrderItem ryso = (RYSOrderItem) disItem;
+				if (disItem.getSku().getId().equals(skuId)) {
+					ryso.setRentTerm(Long.valueOf(rentTerm));
+				}
+			}
+		}
 	}
 
 }
