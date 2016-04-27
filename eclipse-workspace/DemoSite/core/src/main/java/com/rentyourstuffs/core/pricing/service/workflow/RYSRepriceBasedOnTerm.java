4ad091f3fe4018e3f24828e0d69659813c77000d
Prakash Ponali <prakash.ponali@ril.com>
2016-04-27 12:10:19 +0530

    Prakash - fix for the reprice issue.

--- eclipse-workspace/DemoSite/core/src/main/java/com/rentyourstuffs/core/pricing/service/workflow/RYSRepriceBasedOnTerm.java
+++ eclipse-workspace/DemoSite/core/src/main/java/com/rentyourstuffs/core/pricing/service/workflow/RYSRepriceBasedOnTerm.java
@@ -2,29 +2,42 @@ package com.rentyourstuffs.core.pricing.service.workflow;
 
 import java.util.List;
 
+import org.broadleafcommerce.core.catalog.service.dynamic.SkuPricingConsiderationContext;
 import org.broadleafcommerce.core.order.domain.DiscreteOrderItem;
 import org.broadleafcommerce.core.order.domain.Order;
 import org.broadleafcommerce.core.order.domain.OrderItem;
 import org.broadleafcommerce.core.workflow.BaseActivity;
 import org.broadleafcommerce.core.workflow.ProcessContext;
 
+import com.rentyourstuffs.core.order.domain.RYSOrderItem;
+
 public class RYSRepriceBasedOnTerm extends BaseActivity<ProcessContext<Order>> {
 
 	@Override
 	public ProcessContext<Order> execute(ProcessContext<Order> context)
 			throws Exception {
-		Order order = context.getSeedData();
-		for(OrderItem orderItem : order.getOrderItems())
-		{
+		Order order = context.getSeedData();		
 			List<DiscreteOrderItem> discreteOrderItems = order.getDiscreteOrderItems();
 			if(discreteOrderItems != null){
-				for(DiscreteOrderItem discreteOrderItem : discreteOrderItems)
+				if(SkuPricingConsiderationContext.getSkuPricingConsiderationContext() != null)
 				{
-					discreteOrderItem.setPrice(discreteOrderItem.getSku().getRetailPrice());
+					for(DiscreteOrderItem discreteOrderItem : discreteOrderItems)
+					{
+						discreteOrderItem.setPrice(discreteOrderItem.getSku().getRetailPrice());
+					}
 				}
+				else
+				{
+					for(DiscreteOrderItem discreteOrderItem : discreteOrderItems)
+					{
+						if(((RYSOrderItem)discreteOrderItem).getRentPrice() != null)
+						{
+							discreteOrderItem.setPrice(((RYSOrderItem)discreteOrderItem).getRentPrice());
+						}
+					}
+				}
+				
 			}
-		}
-		
 		context.setSeedData(order);
 		return context;
 	}
